4ab4d7df9539514926d097e0230946487a2ba154
WyShadow <luujason11@gmail.com>
2024-01-07 09:24:37 -0600

    connected MongoDB to application

--- backend/config.js
+++ backend/config.js
@@ -1,1 +1,4 @@
 export const PORT = 5555;
+
+export const mongoDBURL =
+'mongodb+srv://luujason11:NmU9dPZA7gtuMqsn@cluster0.lthtylw.mongodb.net/books-collection?retryWrites=true&w=majority'
--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 import express from "express";
-import { PORT } from "./config.js";
+import { PORT, mongoDBURL } from "./config.js";
+import mongoose from "mongoose";
 
 const app = express();
 
@@ -9,6 +10,16 @@ app.get('/', (req, res)=>{
 });
 
 
-app.listen(PORT, ()=>{
-  console.log(`App is listening to port ${PORT}`);
+
+
+mongoose
+.connect(mongoDBURL)
+.then(()=>{
+  console.log(`App is connected  to database`);
+  app.listen(PORT, ()=>{
+    console.log(`App is listening to port ${PORT}`);
+  })
+})
+.catch((error)=>{
+  console.log(error);
 })
