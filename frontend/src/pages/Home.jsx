39553f2a5ad5797cbc57ae2ef947e6da706ead0d
WyShadow <luujason11@gmail.com>
2024-01-08 18:03:54 -0600

    Finished CRUD Applications

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,16 +1,35 @@
-import React from 'react';
-import { useState, useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import MdOutlineAddBox from 'react-icons/md'
-
+import Spinner from '../components/Spinner';
+import { Link } from 'react-router-dom';
+import { AiOutlineEdit } from 'react-icons/ai';
+import { BsInfoCircle } from 'react-icons/bs';
+import { MdOutlineAddBox, MdOutlineDelete } from 'react-icons/md';
 
 const Home = () => {
+  const [loading, setLoading] = useState(false);
+  const [books, setBooks] = useState([]);
+
+  useEffect(()=>{
+    setLoading(true);
+    axios.get("http://localhost:5555/books").then((response)=>{
+      console.log(response);
+      setBooks(response.data.data);
+      setLoading(false);
+    })
+    .catch((err)=>{
+      console.log(err);
+      setLoading(false);
+    })
+
+  },[]);
+  
   return (
     <div className='p-4'>
       <div className="flex justify-between items-center">
         <h1 className="text-3xl my-8">Book List</h1>
         <Link to='/books/create'>
-          <MdOutlineAddBox className="text-sky-800 text4xl" />
+          <MdOutlineAddBox className="text-sky-800 text-4xl" />
         </Link>
       </div>
       {loading ? (
@@ -27,25 +46,28 @@ const Home = () => {
             </tr>
           </thead>
           <tbody>
-            <tr>
-              <td className="border border-slate-700 rounded-md text-center">1</td>
-              <td className="border border-slate-700 rounded-md text-center">Harry Poter</td>
-              <td className="border border-slate-700 rounded-md text-center max-md:hidden">Jason Luu</td>
-              <td className="border border-slate-700 rounded-md text-center max-md:hidden">1945</td>
-              <td className="border border-slate-700 rounded-md text-center">
-                <div className="flex justify-center gap-x-4">
-                  <Link to={`/books/details/${book._id}`}>
-                    <BsInfoCircle className="text-2xl text-green-800" />
-                  </Link>
-                  <Link to={`/books/edit/${book._id}`}>
-                    <AiOutlineEdit className="text-2xl text-yellow-600" />
-                  </Link>
-                  <Link to={`/books/delete/${book._id}`}>
-                    <MdOutlineDelete className="text-2xl text-red-600" />
-                  </Link>
-                </div>
-              </td>
-            </tr>
+            {books.map((book, index)=>{
+              return(
+              <tr key={book._id} className='h-8'>
+                <td className="border border-slate-700 rounded-md text-center"> {index + 1}</td>
+                <td className="border border-slate-700 rounded-md text-center"> {book.title}  </td>
+                <td className="border border-slate-700 rounded-md text-center max-md:hidden">{book.author}</td>
+                <td className="border border-slate-700 rounded-md text-center max-md:hidden">{book.publishYear}</td>
+                <td className="border border-slate-700 rounded-md text-center">
+                  <div className="flex justify-center gap-x-4">
+                    <Link to={`/books/details/${book._id}`}>
+                      <BsInfoCircle className="text-2xl text-green-800" />
+                    </Link>
+                    <Link to={`/books/edit/${book._id}`}>
+                      <AiOutlineEdit className="text-2xl text-yellow-600" />
+                    </Link>
+                    <Link to={`/books/delete/${book._id}`}>
+                      <MdOutlineDelete className="text-2xl text-red-600" />
+                    </Link>
+                  </div>
+                </td>
+              </tr>
+              )})}
           </tbody>
         </table>
       )}
